2bdc17bbbef0e58ca2d36349280170d22ce9c48e
NanyonjoShamsa <shamsananyonjo94@gmail.com>
2024-09-23 23:51:51 +0300

    Update and rename exampleFive (1).java to  Loop.java

--- Loop.java
+++ Loop.java
@@ -1,6 +1,6 @@
 //Question 3: Write a Java program that prints all even numbers
 // from 1 to 20 using a for loop.
-public class exampleFive {
+public class loop exercise {
     public static void main(String[] args) {
         for (int i = 1; i <= 20; i++) {
             if (i % 2 == 0) {
