d89ef6c025178654c29c35f4dcf3424b21e1b382
NanyonjoShamsa <shamsananyonjo94@gmail.com>
2024-09-24 00:04:26 +0300

    Update and rename exampleSeven (1).java to Switch 2.java

--- Switch 2.java
+++ Switch 2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class exampleSeven {
+public class exercise switch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number (1-7): ");
